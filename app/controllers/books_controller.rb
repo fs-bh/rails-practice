a5fccad3a1a9e0a97ac8d135193f920b21d1a3dc
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-18 15:31:47 -0400

    feat: add ability to add author to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,32 +2,19 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 	end
-	def new
-		@book = Book.new
-	end
-	def create
-		@book = Book.new(strong_params(:title, :pages))
-		if @book.save
-			redirect_to @book
-		else
-			render :new
-		end
-	end
 	def show
 		@book = get_instance
-	end
-	def edit
-		@book = get_instance
+		@authors = Author.all
 	end
 	def update
 		# TODO: Add validation before (or during?) running the update.
 		book = get_instance
-		book.update(strong_params(:title, :pages))
+		book.authors << Author.find(params[:book][:authors])
 		redirect_to book
 	end
-
+	
 	private
-
+	
 	def strong_params(*args)
 		params.require(:book).permit(*args)
 	end
@@ -35,3 +22,4 @@ class BooksController < ApplicationController
 		Book.find(params[:id])
 	end
 end
+
