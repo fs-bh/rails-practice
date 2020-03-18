642ca54e89209882f4a15872ab1f4e63fcc21618
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-18 12:14:40 -0400

    build: create Author and Book routes, models, controllers, view-folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'author/index'
+  get 'author/new'
+  get 'author/create'
+  get 'author/show'
+  get 'author/edit'
+  get 'author/update'
+  get 'author/delete'
+  resources :books
+  resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
